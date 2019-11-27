9cf10c319fe3c63b2f3b531e80c844bfc46f8447
Ryan Pivovar <ryanpivovar@gmail.com>
2019-11-26 18:43:12 -0800

    add lint script, remove react-plugin extend

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   "extends": [
     "eslint:recommended",
-    "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
   ],
   "plugins": ["react", "@typescript-eslint"],
