b5063c6bcbab3460fdd54a8253879c35d596cb38
Ryan Pivovar <ryanpivovar@gmail.com>
2019-11-29 17:49:34 -0800

    import only render method from ReactDOM

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 
 import { App } from './components/App';
 
-ReactDOM.render(
+render(
   <App compiler='TypeScript' framework='React' />,
   document.getElementById('example')
 );
