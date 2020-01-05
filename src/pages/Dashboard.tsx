98dd582ddc0a019af5e3d3e7d4a673eb078cc71f
Ryan Pivovar <ryanpivovar@gmail.com>
2020-01-04 22:36:17 -0500

    set up Dashboard as a styled-component

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Timer } from '@components/Timer';
+import StyledDashboard from '@pages/styles/Dashboard.styled';
 
 export class Dashboard extends React.Component {
-    render(): JSX.Element { return <Timer />; }
+render(): JSX.Element { return (
+    <StyledDashboard>
+        <Timer />
+    </StyledDashboard>
+  );
+}
 }
