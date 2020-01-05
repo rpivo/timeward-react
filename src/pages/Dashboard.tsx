26c911223982f7bb087b1c914ef807cb1f4633e5
Ryan Pivovar <ryanpivovar@gmail.com>
2020-01-05 00:33:28 -0500

    add public modifiers

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,7 +3,7 @@ import { Timer } from '@components/Timer';
 import StyledDashboard from '@pages/styles/Dashboard.styled';
 
 export class Dashboard extends React.Component {
-  render(): JSX.Element { return (
+  public render(): JSX.Element { return (
       <StyledDashboard>
           <Timer />
       </StyledDashboard>
