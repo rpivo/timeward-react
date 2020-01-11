13007289db66fe6b4f2f10a64fa3980a5a7ecaa6
Ryan Pivovar <ryanpivovar@gmail.com>
2020-01-11 00:21:09 -0500

    added StyledPieChart

---
+++ src/components/styles/StyledPieChart.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyledPieChart = styled.div`
+  background: #567;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+`;
+
+export default StyledPieChart;
