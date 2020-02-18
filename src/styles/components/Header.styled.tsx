7ce93d7a468e1e9bcb840801338809b8aff75b44
Ryan Pivovar <ryanpivovar@gmail.com>
2020-02-17 21:37:20 -0500

    add styles to header

--- src/styles/components/Header.styled.tsx
+++ src/styles/components/Header.styled.tsx
@@ -5,6 +5,19 @@ const StyledHeader = styled.header`
   background: #345;
   color: white;
   padding: 8px;
+  display: flex;
+  justify-content: space-between;
+
+  h5 {
+    letter-spacing: 0.25rem;
+    margin: 0;
+  }
+
+  nav a {
+    text-decoration: none;
+    color: white;
+    padding-left: 0.75rem;
+  }
 `;
 
 export default StyledHeader;
