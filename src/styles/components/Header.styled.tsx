ca6487cbdca5744c245c6eeecf1e8e27f8d48fc8
Ryan Pivovar <ryanpivovar@gmail.com>
2020-02-24 00:54:25 -0500

    use theme primary color in header styles

--- src/styles/components/Header.styled.tsx
+++ src/styles/components/Header.styled.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const StyledHeader = styled.header`
   width: calc(100% - 16px);
-  background: #345;
+  background: ${(props): string => props.theme.primary};
   color: white;
   padding: 8px;
   display: flex;
