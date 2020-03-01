31dd85b673755c6df0165bc5e3e38d5f5b360213
Ryan Pivovar <ryanpivovar@gmail.com>
2020-03-01 11:19:20 -0500

    formatting

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Dashboard from '@pages/Dashboard';
 import Login from '@pages/Login';
 import Header from '@components/Header';
