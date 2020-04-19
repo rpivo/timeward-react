0021a4f3f46d9ef504ae1ec84bcaba1a7b100b70
Ryan Pivovar <ryanpivovar@gmail.com>
2020-04-19 00:49:47 -0400

    linting and formatting

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,36 +11,36 @@ const Button: React.FC<ButtonProps> = ({ kind, handleClick }: ButtonProps): JSX.
 
   const Start: React.FC = (): JSX.Element =>
     <svg width="15" height="15" viewBox="0 0 50 50">
-      <path d={ buttonPaths.start } transform="translate(54.392 -7.309) rotate(90)" fill="#aaa" />
+      <path d={buttonPaths.start} transform="translate(54.392 -7.309) rotate(90)" fill="#aaa" />
     </svg>;
 
   const Pause: React.FC = (): JSX.Element =>
     <svg width="15" height="15" viewBox="0 0 50 50">
       <g transform="translate(33 404)">
-        <path d={ buttonPaths.pause } transform="translate(-5.034 -404)" fill="#aaa"/>
-        <path d={ buttonPaths.pause } transform="translate(-33 -404)" fill="#aaa"/>
+        <path d={buttonPaths.pause} transform="translate(-5.034 -404)" fill="#aaa" />
+        <path d={buttonPaths.pause} transform="translate(-33 -404)" fill="#aaa" />
       </g>
     </svg>;
 
   const Stop: React.FC = (): JSX.Element =>
     <svg width="15" height="15" viewBox="0 0 50 50">
-      <path d={ buttonPaths.stop } fill="#aaa"/>
+      <path d={buttonPaths.stop} fill="#aaa" />
     </svg>;
 
   const renderSwitch = (buttonType: string): JSX.Element => {
     switch (buttonType) {
-    case 'start':
-      return <Start />;
-    case 'pause':
-      return <Pause />;
-    default:
-      return <Stop />;
+      case 'start':
+        return <Start />;
+      case 'pause':
+        return <Pause />;
+      default:
+        return <Stop />;
     }
   };
 
   return (
-    <StyledButton kind={ kind } onClick={ handleClick }>
-      { renderSwitch(kind) }
+    <StyledButton kind={kind} onClick={handleClick}>
+      {renderSwitch(kind)}
     </StyledButton>
   );
 };
